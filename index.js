920fdf8fda262db3d125f19fe5f94b4221645716
bensy111 <benjamin.kiwalabye@students.mak.ac.ug>
2023-07-22 01:29:42 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+//window.alert(hello);
+//document.getElementById("demo").innerHTML = 5 + 6;
+
+/*function myFunction() {
+  document.getElementById("author").innerHTML = "Paragraph changed.";
+}*/
+/*document.write("hello world");
+var1 = 10;
+var2 = 20;
+
+var3 = var1 + var2;
+window.alert(
+  "welcom benjamin it could be your first program for javascript but never mind the sky is the limit buddy"
+);*/
+function myFuction() {
+  alert("hello");
+}
